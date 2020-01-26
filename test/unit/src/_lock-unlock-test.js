c04bfd0562b46057fd1fb7048f3caddb2c570a91
Ryan Block <rblock@gmail.com>
2020-01-26 12:02:02 -0800

    Unifies lock/unlock methods

--- test/unit/src/_lock-unlock-test.js
+++ test/unit/src/_lock-unlock-test.js
@@ -14,15 +14,28 @@ let tiny = {
     callback(null, {body:'foo'})
   }
 }
-let unlock = proxyquire('../../../src/_unlock', {
+let lockUnlock = proxyquire('../../../src/_lock-unlock', {
   './util/session': session,
   './_locks': locks,
   'tiny-json-http': tiny
 })
 
+let lock = lockUnlock.bind({}, 'lock')
+let unlock = lockUnlock.bind({}, 'unlock')
+
 test('Returns a Promise or uses continuation passing', t => {
-  t.plan(5)
-  let isPromise = unlock() instanceof Promise
+  t.plan(10)
+  // Lock
+  let isPromise = lock() instanceof Promise
+  t.ok(isPromise, 'Promise returned (without params)')
+  isPromise = lock('foo') instanceof Promise
+  t.ok(isPromise, 'Promise returned (with params)')
+  lock(() => (t.pass('Executed callback (without params)')))
+  lock(null, () => (t.pass('Executed callback (null params)')))
+  lock('foo', () => (t.pass('Executed callback (with params)')))
+
+  // Unlock
+  isPromise = unlock() instanceof Promise
   t.ok(isPromise, 'Promise returned (without params)')
   isPromise = unlock('foo') instanceof Promise
   t.ok(isPromise, 'Promise returned (with params)')
@@ -32,7 +45,16 @@ test('Returns a Promise or uses continuation passing', t => {
 })
 
 test('Calls August endpoint with correct params (passed lockID)', t => {
-  t.plan(3)
+  t.plan(6)
+  // Lock
+  lock('myLockID', (err, result) => {
+    if (err) t.fail(err)
+    t.equal(params.url, 'https://api-production.august.com/remoteoperate/myLockID/lock', 'Valid August endpoint')
+    t.equal(params.headers['Content-Length'], 0, 'Appended zero content-length to request headers')
+    t.ok(result, 'Returned result')
+  })
+
+  // Unlock
   unlock('myLockID', (err, result) => {
     if (err) t.fail(err)
     t.equal(params.url, 'https://api-production.august.com/remoteoperate/myLockID/unlock', 'Valid August endpoint')
@@ -42,7 +64,15 @@ test('Calls August endpoint with correct params (passed lockID)', t => {
 })
 
 test('Fails when no lockID is passed and multiple locks are returned by the API', t => {
-  t.plan(1)
+  t.plan(2)
+  // Lock
+  lockData.body['baz'] = 'buz'
+  lock(null, (err) => {
+    delete lockData.body['baz']
+    t.ok(err, `Error returned: ${err}`)
+  })
+
+  // Unlock
   lockData.body['baz'] = 'buz'
   unlock(null, (err) => {
     delete lockData.body['baz']
@@ -51,7 +81,16 @@ test('Fails when no lockID is passed and multiple locks are returned by the API'
 })
 
 test('Calls August endpoint with correct params (no lockID passed)', t => {
-  t.plan(3)
+  t.plan(6)
+  // Lock
+  lock(null, (err, result) => {
+    if (err) t.fail(err)
+    t.equal(params.url, 'https://api-production.august.com/remoteoperate/foo/lock', 'Valid August endpoint')
+    t.equal(params.headers['Content-Length'], 0, 'Appended zero content-length to request headers')
+    t.ok(result, 'Returned result')
+  })
+
+  // Unlock
   unlock(null, (err, result) => {
     if (err) t.fail(err)
     t.equal(params.url, 'https://api-production.august.com/remoteoperate/foo/unlock', 'Valid August endpoint')
