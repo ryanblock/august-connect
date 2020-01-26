d0a56183e70bff6261026457388b10bd3f6884f8
Ryan Block <rblock@gmail.com>
2020-01-26 13:32:04 -0800

    Clean up locks call with bind, update integration tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 let authorize = require('./_authorize')
 let lockUnlock = require('./_lock-unlock')
-let _locks = require('./_locks')
+let locks = require('./_locks')
 let status = require('./_status')
 
 module.exports = {
   authorize,
   lock: lockUnlock.bind({}, 'lock'),
-  locks: callback => {
-    _locks({internal: false}, callback)
-  },
+  locks: locks.bind({}, {internal: false}),
   status,
   unlock: lockUnlock.bind({}, 'unlock'),
   validate: authorize
