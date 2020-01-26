75bb0e6749a23df8b68d9283f2ae2f10068d739b
Ryan Block <rblock@gmail.com>
2020-01-26 13:23:16 -0800

    Consolidate lock/unlock integration tests

--- test/integration/src/_lock-unlock-test.js
+++ test/integration/src/_lock-unlock-test.js
@@ -1,12 +1,19 @@
 let test = require('tape')
-let unlock = require('../../../src/_unlock')
+let august = require('../../../src/')
 let isCI = process.env.CI
 // eslint-disable-next-line
 if (!isCI) require('dotenv').config()
 
 test('Unlock a lock', async t => {
   t.plan(1)
-  let unlocked = await unlock()
+  let unlocked = await august.unlock()
   console.log(unlocked)
   t.equal(unlocked.status, 'kAugLockState_Unlocked', `Lock unlocked`)
+})
+
+test('Lock a lock', async t => {
+  t.plan(1)
+  let locked = await august.lock()
+  console.log(locked)
+  t.equal(locked.status, 'kAugLockState_Locked', `Lock locked`)
 })
