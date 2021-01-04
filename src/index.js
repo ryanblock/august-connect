99ea5c39fa7f5501ef83a0591e16dcb7a7d73a9c
Blaine Miller <techyowl@gmail.com>
2021-01-03 18:13:16 -0800

    Add lock details method for battery info

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ let authorize = require('./_authorize')
 let lockUnlock = require('./_lock-unlock')
 let { external } = require('./_locks')
 let status = require('./_status')
+let details = require('./_details')
 
 module.exports = {
   authorize,
+  details,
   lock: lockUnlock.bind({}, 'lock'),
   locks: external,
   status,
